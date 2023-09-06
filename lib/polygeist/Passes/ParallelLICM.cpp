4a95fc652a3ba55f681dbabc72ca19e20fa1f0eb
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-09-06 16:26:01 +0900

    plicm

--- lib/polygeist/Passes/ParallelLICM.cpp
+++ lib/polygeist/Passes/ParallelLICM.cpp
@@ -306,6 +306,7 @@ bool below(AffineExpr expr, size_t numDim, ValueRange operands, int64_t val) {
   return false;
 }
 
+namespace mlir::polygeist {
 bool isSpeculatable(Operation *op) {
   if (auto memInterface = dyn_cast<MemoryEffectOpInterface>(op)) {
     // If the op has no side-effects, it is speculatable.
@@ -354,9 +355,10 @@ bool isSpeculatable(Operation *op) {
   // Recurse into the regions and ensure that all nested ops can also be moved.
   for (Region &region : op->getRegions())
     for (Operation &op : region.getOps())
-      if (!isSpeculatable(&op))
+      if (!polygeist::isSpeculatable(&op))
         return false;
   return true;
+}
 }
 
 void moveParallelLoopInvariantCode(scf::ParallelOp looplike) {
@@ -375,7 +377,7 @@ void moveParallelLoopInvariantCode(scf::ParallelOp looplike) {
     for (Region &region : metaop->getRegions())
       for (Block &block : region)
         for (Operation &op : block.without_terminator())
-          if ((!checkSpeculative || isSpeculatable(&op)) &&
+          if ((!checkSpeculative || polygeist::isSpeculatable(&op)) &&
               canBeParallelHoisted(&op, looplike, willBeMovedSet)) {
             opsToMove.push_back(&op);
             willBeMovedSet.insert(&op);
@@ -387,7 +389,7 @@ void moveParallelLoopInvariantCode(scf::ParallelOp looplike) {
 
   // For all instructions that we found to be invariant, move outside of the
   // loop.
-  if (!llvm::all_of(opsToMove, isSpeculatable)) {
+  if (!llvm::all_of(opsToMove, polygeist::isSpeculatable)) {
     OpBuilder b(looplike);
     Value cond = nullptr;
     for (auto pair : llvm::zip(looplike.getLowerBound(),
@@ -435,7 +437,7 @@ void moveParallelLoopInvariantCode(affine::AffineParallelOp looplike) {
     for (Region &region : metaop->getRegions())
       for (Block &block : region)
         for (Operation &op : block.without_terminator())
-          if ((!checkSpeculative || isSpeculatable(&op)) &&
+          if ((!checkSpeculative || polygeist::isSpeculatable(&op)) &&
               canBeParallelHoisted(&op, looplike, willBeMovedSet)) {
             opsToMove.push_back(&op);
             willBeMovedSet.insert(&op);
@@ -447,7 +449,7 @@ void moveParallelLoopInvariantCode(affine::AffineParallelOp looplike) {
 
   // For all instructions that we found to be invariant, move outside of the
   // loop.
-  if (!llvm::all_of(opsToMove, isSpeculatable)) {
+  if (!llvm::all_of(opsToMove, polygeist::isSpeculatable)) {
     OpBuilder b(looplike);
 
     // TODO properly fill exprs and eqflags
@@ -544,7 +546,7 @@ void moveSerialLoopInvariantCode(scf::ForOp looplike) {
     for (Region &region : metaop->getRegions())
       for (Block &block : region)
         for (Operation &op : block.without_terminator())
-          if ((!checkSpeculative || isSpeculatable(&op)) &&
+          if ((!checkSpeculative || polygeist::isSpeculatable(&op)) &&
               canBeParallelHoisted(&op, looplike, willBeMovedSet,
                                    /*checkAfter*/ true)) {
             opsToMove.push_back(&op);
@@ -557,7 +559,7 @@ void moveSerialLoopInvariantCode(scf::ForOp looplike) {
 
   // For all instructions that we found to be invariant, move outside of the
   // loop.
-  if (!llvm::all_of(opsToMove, isSpeculatable)) {
+  if (!llvm::all_of(opsToMove, polygeist::isSpeculatable)) {
     OpBuilder b(looplike);
     Value cond = b.create<arith::CmpIOp>(looplike.getLoc(), CmpIPredicate::slt,
                                          looplike.getLowerBound(),
@@ -597,7 +599,7 @@ void moveSerialLoopInvariantCode(affine::AffineForOp looplike) {
     for (Region &region : metaop->getRegions())
       for (Block &block : region)
         for (Operation &op : block.without_terminator()) {
-          if ((!checkSpeculative || isSpeculatable(&op)) &&
+          if ((!checkSpeculative || polygeist::isSpeculatable(&op)) &&
               canBeParallelHoisted(&op, looplike, willBeMovedSet,
                                    /*checkAfter*/ true)) {
             opsToMove.push_back(&op);
@@ -611,7 +613,7 @@ void moveSerialLoopInvariantCode(affine::AffineForOp looplike) {
 
   // For all instructions that we found to be invariant, move outside of the
   // loop.
-  if (!llvm::all_of(opsToMove, isSpeculatable)) {
+  if (!llvm::all_of(opsToMove, polygeist::isSpeculatable)) {
     OpBuilder b(looplike);
 
     // TODO properly fill exprs and eqflags
